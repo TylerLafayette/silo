0477c0740e63a2f640be9dd87317f2890f33af11
Tyler Lafayette <tylerlafayette@gmail.com>
2020-11-25 15:10:31 -0800

    Make MatrixTransformerBuilder functions public because I forgot

--- silo-transform/src/matrix.rs
+++ silo-transform/src/matrix.rs
@@ -18,7 +18,7 @@ pub struct MatrixTransformerRow {
 
 impl MatrixTransformerRow {
     /// Creates and returns a new MatrixTransformerRow.
-    fn new(binary_fields: HashMap<String, bool>, int_fields: HashMap<String, i16>) -> Self {
+    pub fn new(binary_fields: HashMap<String, bool>, int_fields: HashMap<String, i16>) -> Self {
         Self {
             binary_fields,
             int_fields,
@@ -34,7 +34,7 @@ pub struct MatrixTransformerBuilder {
 
 impl MatrixTransformerBuilder {
     /// Creates and returns a new MatrixTransformerBuilder.
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             __binary_fields: vec![],
             __int_fields: vec![],
@@ -43,25 +43,25 @@ impl MatrixTransformerBuilder {
     }
 
     /// Adds an int field to the matrix.
-    fn with_int_field(mut self, field_name: &str) -> Self {
+    pub fn with_int_field(mut self, field_name: &str) -> Self {
         self.__int_fields.push(field_name.into());
         self
     }
 
     /// Adds a binary field to the matrix.
-    fn with_binary_field(mut self, field_name: &str) -> Self {
+    pub fn with_binary_field(mut self, field_name: &str) -> Self {
         self.__binary_fields.push(field_name.into());
         self
     }
 
     /// Sets the output type of the matrix with a MatrixOutputType enum.
-    fn output_as(mut self, output_type: MatrixOutputType) -> Self {
+    pub fn output_as(mut self, output_type: MatrixOutputType) -> Self {
         self.__output_type = output_type;
         self
     }
 
     /// Builds the MatrixTransformer.
-    fn build(self) -> MatrixTransformer {
+    pub fn build(self) -> MatrixTransformer {
         MatrixTransformer {
             binary_fields: self.__binary_fields,
             int_fields: self.__int_fields,
