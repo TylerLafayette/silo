ca7929c5329c74b023b9371f13e0c5030b70d808
Tyler Lafayette <tylerlafayette@gmail.com>
2020-11-25 15:08:04 -0800

    :tada: Matrix transformer now works for TSV formats!

--- silo-transform/src/matrix.rs
+++ silo-transform/src/matrix.rs
@@ -2,22 +2,34 @@ use std::collections::HashMap;
 
 /// Specifies different output types for a matrix.
 pub enum MatrixOutputType {
+    /// Outputs the matrix as a JSON value.
     Json,
+    /// Outputs the matrix as a CSV.
     Csv,
+    /// Outputs the matrix as a tab-separated file.
     Tsv,
-}
-
-/// A builder for creating matrix transformers.
-pub struct MatrixTransformerBuilder {
-    __binary_fields: Vec<String>,
-    __int_fields: Vec<String>,
-    __output_type: MatrixOutputType,
 }
 
 /// Contains both int and binary field vectors.
 pub struct MatrixTransformerRow {
     binary_fields: HashMap<String, bool>,
     int_fields: HashMap<String, i16>,
+}
+
+impl MatrixTransformerRow {
+    /// Creates and returns a new MatrixTransformerRow.
+    fn new(binary_fields: HashMap<String, bool>, int_fields: HashMap<String, i16>) -> Self {
+        Self {
+            binary_fields,
+            int_fields,
+        }
+    }
+}
+/// A builder for creating matrix transformers.
+pub struct MatrixTransformerBuilder {
+    __binary_fields: Vec<String>,
+    __int_fields: Vec<String>,
+    __output_type: MatrixOutputType,
 }
 
 impl MatrixTransformerBuilder {
@@ -53,6 +65,7 @@ impl MatrixTransformerBuilder {
         MatrixTransformer {
             binary_fields: self.__binary_fields,
             int_fields: self.__int_fields,
+            output_type: self.__output_type,
         }
     }
 }
@@ -61,9 +74,74 @@ impl MatrixTransformerBuilder {
 pub struct MatrixTransformer {
     binary_fields: Vec<String>,
     int_fields: Vec<String>,
+    output_type: MatrixOutputType,
 }
 
 impl MatrixTransformer {
     /// Generates and returns a matrix.
-    pub fn generate(self, rows: Vec<MatrixTransformerRow>) {}
+    pub fn generate(self, rows: Vec<MatrixTransformerRow>) -> Result<String, std::io::Error> {
+        let mut matrix_output = String::from("");
+
+        rows.iter().for_each(|row| match self.output_type {
+            // No support yet for types other than TSV.
+            // TODO: implement those output types :)
+            _ => self.append_tsv_row(&mut matrix_output, &row),
+        });
+
+        Ok(matrix_output)
+    }
+
+    /// Formats a row as TSV and appends it to the builder with a trailing newline.
+    fn append_tsv_row(&self, builder: &mut String, row: &MatrixTransformerRow) {
+        self.int_fields
+            .iter()
+            .for_each(|field_name| match row.int_fields.get(field_name) {
+                Some(value) => builder.push_str(&format!("{}\t", value)),
+                _ => builder.push_str("NULL\t"),
+            });
+
+        self.binary_fields
+            .iter()
+            .for_each(|field_name| match row.binary_fields.get(field_name) {
+                Some(true) => builder.push_str("1\t".into()),
+                _ => builder.push_str("0\t"),
+            });
+
+        builder.push_str("\n");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_matrix() {
+        let transformer = MatrixTransformerBuilder::new()
+            .with_int_field("age")
+            .with_binary_field("migraine")
+            .build();
+        let output = transformer.generate(vec![]).unwrap();
+        assert!(output == "");
+    }
+
+    #[test]
+    fn one_element_matrix() {
+        let transformer = MatrixTransformerBuilder::new()
+            .with_int_field("age")
+            .with_binary_field("migraine")
+            .output_as(MatrixOutputType::Tsv)
+            .build();
+
+        let mut int_fields: HashMap<String, i16> = HashMap::new();
+        int_fields.insert("age".into(), 24);
+
+        let mut binary_fields: HashMap<String, bool> = HashMap::new();
+        binary_fields.insert("migraine".into(), true);
+
+        let row = MatrixTransformerRow::new(binary_fields, int_fields);
+
+        let output = transformer.generate(vec![row]).unwrap();
+        assert!(output == "24\t1\t\n");
+    }
 }
